6613e0e8150ea01824c333cb388d6dc42dd1b3c0
Dominic <Dominic@PROJECTX>
2024-03-12 19:49:17 -0400

    update

--- Sources/Renderer/Renderer2D.cpp
+++ Sources/Renderer/Renderer2D.cpp
@@ -2,5 +2,28 @@
 
 namespace Engine
 {
+	struct Renderer2DData
+	{
+		std::shared_ptr<VertexArray> QuadVertexArray;
+		std::shared_ptr<VertexBuffer> QuadVertexBuffer;
+		std::shared_ptr<Shader> QuadShader;
+		std::shared_ptr<Texture> QuadTexture;
+	};
 
+	static Renderer2DData s_Data;
+
+	void Renderer2D::Init()
+	{
+
+	}
+
+	void Renderer2D::DrawQuad(const glm::mat4& transform, const glm::vec4& color)
+	{
+		
+	}
+
+	void Renderer2D::DrawQuad(const glm::mat4& transform, const Texture& texture)
+	{
+
+	}
 }
--- Sources/Renderer/Renderer2D.h
+++ Sources/Renderer/Renderer2D.h
@@ -1,9 +1,26 @@
 #pragma once
 
+#include "Core/VertexArray.h"
+#include "Buffers/ElementBuffer.h"
+
+#include "Core/Texture.h"
+#include "Core/Shader.h"
+#include "Core/Primitives.h"
+
+#include <glm.hpp>
+
 namespace Engine
 {
 	class Renderer2D
 	{
+	public:
+		static void Init();
+
+		static void DrawQuad(const glm::mat4& transform, const glm::vec4& color);
+		static void DrawQuad(const glm::mat4& transform, const Texture& texture);
+
+	private:
+		Quad quad;
 
 	};
 }
